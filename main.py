a121147f92feb3ff3850167021f2979ca5291c41
Bibek Acharya <bibekacharya2059@gmail.com>
2025-05-17 21:49:34 +0545

    updated

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#discord bot
 import discord
 from discord.ext import commands
 import logging
@@ -6,6 +5,17 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-token = os.getenv('')
+token = os.getenv('DISCORD_TOKEN')
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"We are ready to go in, {bot.user.name}")
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
