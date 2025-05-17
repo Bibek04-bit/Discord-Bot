e449f42c70dd5e044502f90822511d9eccd37e43
Bibek Acharya <bibekacharya2059@gmail.com>
2025-05-17 22:10:06 +0545

    updated

--- main.py
+++ main.py
@@ -17,5 +17,27 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f"We are ready to go in, {bot.user.name}")
+    
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server {member.name}")
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if "shit" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} - mukh nachad muji!")
+
+    await bot.process_commands(message)
+    
+    
+@bot.command()
+async def hello(ctx):
+  await ctx.send(f"Hello {ctx.author.mention}!")
+  
+  
+  
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
