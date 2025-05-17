4417a4a6ad18c9e6e12737d034b05f302828a55a
Bibek Acharya <bibekacharya2059@gmail.com>
2025-05-17 21:18:05 +0545

    updated

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #discord bot
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv('')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
